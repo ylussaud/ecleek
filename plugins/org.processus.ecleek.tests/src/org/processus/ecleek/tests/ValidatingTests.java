2401fea65a4cc97012e174312e772483918e67f9
Yvan Lussaud <yvan.lussaud@obeo.fr>
2014-12-28 15:56:27 +0100

    Added validation tests.

--- plugins/org.processus.ecleek.tests/src/org/processus/ecleek/tests/ValidatingTests.java
+++ plugins/org.processus.ecleek.tests/src/org/processus/ecleek/tests/ValidatingTests.java
@@ -3,6 +3,7 @@ package org.processus.ecleek.tests;
 import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.ecore.util.Diagnostician;
 import org.eclipse.xtext.junit4.AbstractXtextTests;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.processus.ecleek.LeekStandaloneSetup;
 import org.processus.ecleek.leek.Script;
@@ -21,7 +22,7 @@ public class ValidatingTests extends AbstractXtextTests {
 	}
 
 	@Test
-	public void checkReturnStatementIsInFunctionDeclaration() throws Exception {
+	public void checkReturnStatementIsInFunctionDeclarationNotInFunction() throws Exception {
 		final Script script = getScript("return 1;");
 
 		final Diagnostic diagnostic = Diagnostician.INSTANCE.validate(script);
@@ -29,9 +30,25 @@ public class ValidatingTests extends AbstractXtextTests {
 		assertDiagnostic(diagnostic.getChildren().get(0),Diagnostic.ERROR, "ReturnStatement must be in a FunctionDeclaration");
 	}
 
+	@Test
+	public void checkReturnStatementIsInFunctionDeclarationInFunction() throws Exception {
+		final Script script = getScript("function f() {return 1;}");
+
+		final Diagnostic diagnostic = Diagnostician.INSTANCE.validate(script);
+		assertEquals(0, diagnostic.getChildren().size());
+	}
+
+	@Test
+	public void checkReturnStatementIsInFunctionDeclarationInFunctionNested() throws Exception {
+		final Script script = getScript("function f() {if (true) {return 1;}}");
+
+		final Diagnostic diagnostic = Diagnostician.INSTANCE.validate(script);
+		assertEquals(0, diagnostic.getChildren().size());
+	}
+
 	
 	@Test
-	public void checkBreakStatementIsInLoop() throws Exception {
+	public void checkBreakStatementIsInLoopNotInLoop() throws Exception {
 		final Script script = getScript("break;");
 
 		final Diagnostic diagnostic = Diagnostician.INSTANCE.validate(script);
@@ -40,12 +57,68 @@ public class ValidatingTests extends AbstractXtextTests {
 	}
 
 	@Test
-	public void checkContinueStatementIsInLoop() throws Exception {
+	public void checkBreakStatementIsInLoopInWhile() throws Exception {
+		final Script script = getScript("while(true) {break;}");
+
+		final Diagnostic diagnostic = Diagnostician.INSTANCE.validate(script);
+		assertEquals(0, diagnostic.getChildren().size());
+	}
+
+	@Test
+	public void checkBreakStatementIsInLoopInWhileNested() throws Exception {
+		final Script script = getScript("while(true) {if(true){break;}}");
+
+		final Diagnostic diagnostic = Diagnostician.INSTANCE.validate(script);
+		assertEquals(0, diagnostic.getChildren().size());
+	}
+
+	@Test
+	@Ignore
+	public void checkBreakStatementIsInLoopInFor() throws Exception {
+		fail("Not implemented yet.");
+	}
+
+	@Test
+	@Ignore
+	public void checkBreakStatementIsInLoopInForNested() throws Exception {
+		fail("Not implemented yet.");
+	}
+
+	@Test
+	public void checkContinueStatementIsInLoopNotInLoop() throws Exception {
 		final Script script = getScript("continue;");
 
 		final Diagnostic diagnostic = Diagnostician.INSTANCE.validate(script);
 		assertEquals(1, diagnostic.getChildren().size());
 		assertDiagnostic(diagnostic.getChildren().get(0),Diagnostic.ERROR, "ContinueStatement must be in a For or a While");
+	}
+
+	@Test
+	public void checkContinueStatementIsInLoopNotInWhile() throws Exception {
+		final Script script = getScript("while(true){continue;}");
+
+		final Diagnostic diagnostic = Diagnostician.INSTANCE.validate(script);
+		assertEquals(0, diagnostic.getChildren().size());
+	}
+
+	@Test
+	public void checkContinueStatementIsInLoopNotInWhileNested() throws Exception {
+		final Script script = getScript("while(true){if(true){continue;}}");
+
+		final Diagnostic diagnostic = Diagnostician.INSTANCE.validate(script);
+		assertEquals(0, diagnostic.getChildren().size());
+	}
+
+	@Test
+	@Ignore
+	public void checkContinueStatementIsInLoopNotInFor() throws Exception {
+		fail("Not implemented yet.");
+	}
+
+	@Test
+	@Ignore
+	public void checkContinueStatementIsInLoopNotInForNested() throws Exception {
+		fail("Not implemented yet.");
 	}
 
 	@Test
